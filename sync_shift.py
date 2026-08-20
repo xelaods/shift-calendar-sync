1639aac7240c08617a3a0a56f82dcff4356086c9
xelao <xelaods@users.noreply.github.com>
2026-08-21 00:57:22 +0900

    Change sync range to 5 days

--- sync_shift.py
+++ sync_shift.py
@@ -19,7 +19,7 @@ if hasattr(sys.stdout, 'reconfigure'):
 LOGIN_ID = os.getenv("LOGIN_ID", "0332388")
 PASSWORD = os.getenv("PASSWORD", "hs628496")
 GAS_WEB_APP_URL = os.getenv("GAS_WEB_APP_URL", "https://script.google.com/macros/s/AKfycbxhVGQlXnysiCC_yeYNRz8O5hNd_TI3qwE8dQsvYD_5fFkl5OAHTWnB320jptjapRv9/exec")
-SYNC_DAYS = int(os.getenv("SYNC_DAYS", "7"))
+SYNC_DAYS = int(os.getenv("SYNC_DAYS", "5"))
 
 LOGIN_URL = "https://shifucon.ppihgroup.com/staffpage/"
 LOGIN_ACTION_URL = "https://shifucon.ppihgroup.com/frontparts/login_check.php"
